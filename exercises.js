12d9e70eec612b87f5c20234500790e0cddad7b4
Jared Phillips <jaredtphillips@me.com>
2013-06-17 14:36:34 -0400

    Second Solution Complete

--- exercises.js
+++ exercises.js
@@ -10,3 +10,19 @@ var partner = "Rachel";
 
 var future = ("You will be a " + jobTitle + " in " + geoLocation + ", and married to " + partner + " with " + children + " kids.")
 console.log(future)
+
+// Forgot how old you are? Calculate it!
+
+// Store the current year in a variable.
+// Store your birth year in a variable.
+// Calculate your 2 possible ages based on the stored values.
+// Output them to the screen like so: "You are either NN or NN", substituting the values.
+
+var currentYear = 2013;
+var birthdayYear = 1978;
+
+var age = currentYear - birthdayYear;
+var possible = age - 1;
+
+var answerToAge = ("You are either " + age + " or " + possible)
+console.log(answerToAge)
