efffc073bad8c5e03fd3f4bb87ce27cc7b8e9ee3
Jared Phillips <jaredtphillips@me.com>
2013-06-17 14:25:35 -0400

    First Commit

---
+++ exercises.js
@@ -0,0 +1,11 @@
+// Why pay a fortune teller when you can just program your fortune yourself?
+
+// Store the following into variables: number of children, partner's name, geographic location, job title.
+// Output your fortune to the screen like so: "You will be a X in Y, and married to Z with N kids."
+
+var children = 1;
+var jobTitle = "programmer";
+var geoLocation = "New York";
+var partner = "Rachel";
+
+console.log("You will be a " + jobTitle + " in " + geoLocation + ", and married to " + partner + " with " + children + " kids.")
