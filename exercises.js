4f9595dcfa02c90a3479619d91dd45cad39e6dd0
Jared Phillips <jaredtphillips@me.com>
2013-06-17 15:25:30 -0400

    Second set of exercises complete

--- exercises.js
+++ exercises.js
@@ -3,13 +3,13 @@
 // Store the following into variables: number of children, partner's name, geographic location, job title.
 // Output your fortune to the screen like so: "You will be a X in Y, and married to Z with N kids."
 
-var children = 1;
-var jobTitle = "programmer";
-var geoLocation = "New York";
-var partner = "Rachel";
+// var children = 1;
+// var jobTitle = "programmer";
+// var geoLocation = "New York";
+// var partner = "Rachel";
 
-var future = ("You will be a " + jobTitle + " in " + geoLocation + ", and married to " + partner + " with " + children + " kids.")
-console.log(future)
+// var future = ("You will be a " + jobTitle + " in " + geoLocation + ", and married to " + partner + " with " + children + " kids.")
+// console.log(future)
 
 // Forgot how old you are? Calculate it!
 
@@ -18,14 +18,14 @@ console.log(future)
 // Calculate your 2 possible ages based on the stored values.
 // Output them to the screen like so: "You are either NN or NN", substituting the values.
 
-var currentYear = 2013;
-var birthdayYear = 1978;
+// var currentYear = 2013;
+// var birthdayYear = 1978;
 
-var age = currentYear - birthdayYear;
-var possible = age - 1;
+// var age = currentYear - birthdayYear;
+// var possible = age - 1;
 
-var answerToAge = ("You are either " + age + " or " + possible)
-console.log(answerToAge)
+// var answerToAge = ("You are either " + age + " or " + possible)
+// console.log(answerToAge)
 
 // Ever wonder how much a "lifetime supply" of your favorite snack is? Wonder no more!
 
@@ -35,14 +35,14 @@ console.log(answerToAge)
 // Calculate how many you would eat total for the rest of your life.
 // Output the result to the screen like so: "You will need NN to last you until the ripe old age of X".
 
-var favouriteSnack = "Fruit Roll-Ups"
-var currentAge = 35;
-var maximumAge = 99;
-var amountPerDay = 2;
-var lifeTimeSupplyCalculated = (maximumAge - currentAge) * (365 * amountPerDay);
+// var favouriteSnack = "Fruit Roll-Ups"
+// var currentAge = 35;
+// var maximumAge = 99;
+// var amountPerDay = 2;
+// var lifeTimeSupplyCalculated = (maximumAge - currentAge) * (365 * amountPerDay);
 
-var lifeTimeSupply = ("You will need " + lifeTimeSupplyCalculated + " " + favouriteSnack + " to last you until the ripe old age of " + maximumAge )
-console.log(lifeTimeSupply)
+// var lifeTimeSupply = ("You will need " + lifeTimeSupplyCalculated + " " + favouriteSnack + " to last you until the ripe old age of " + maximumAge )
+// console.log(lifeTimeSupply)
 
 // Why pay a fortune teller when you can just program your fortune yourself?
 
@@ -51,13 +51,13 @@ console.log(lifeTimeSupply)
 // outputs your fortune to the screen like so: "You will be a X in Y, and married to Z with N kids."
 // Call that function 3 times with 3 different values for the arguments.
 
-function tellFortune(numChildren, partnerName, location, jobTitle) {
-  console.log("You will be a " + jobTitle + " in " + location + ", and married to " + partnerName + " with " + numChildren + " kids.")
-}
+// function tellFortune(numChildren, partnerName, location, jobTitle) {
+//   console.log("You will be a " + jobTitle + " in " + location + ", and married to " + partnerName + " with " + numChildren + " kids.")
+// }
 
-tellFortune(4, "Rachel", "New York", "Programmer")
-tellFortune(1, "Suzy", "Los Angeles", "Movie Star")
-tellFortune(10, "Kim Kardashinon", "New York", "Programmer")
+// tellFortune(4, "Rachel", "New York", "Programmer")
+// tellFortune(1, "Suzy", "Los Angeles", "Movie Star")
+// tellFortune(10, "Kim Kardashinon", "New York", "Programmer")
 
 // Forgot how old you are? Calculate it!
 
@@ -68,15 +68,31 @@ tellFortune(10, "Kim Kardashinon", "New York", "Programmer")
 // Call the function three times with different sets of values.
 // Bonus: Figure out how to get the current year in JavaScript instead of passing it in.
 
-function calculateAge(birthYear, currentYear) {
-	var age = (currentYear - birthYear)
-	var alsoPossible = (age - 1)
-	console.log("You are either " + age + " or " + alsoPossible )
+// function calculateAge(birthYear, currentYear) {
+// 	var age = (currentYear - birthYear)
+// 	var alsoPossible = (age - 1)
+// 	console.log("You are either " + age + " or " + alsoPossible )
+// }
+
+// calculateAge(1978, 2013)
+// calculateAge(1953, 2013)
+// calculateAge(1995, 2013)
+
+// Ever wonder how much a "lifetime supply" of your favorite snack is? Wonder no more!
+
+// Write a function named calculateSupply that:
+// takes 2 arguments: age, amount per day.
+// calculates the amount consumed for rest of the life (based on a constant max age).
+// outputs the result to the screen like so: "You will need NN to last you until the ripe old age of X"
+// Call that function three times, passing in different values each time.
+// Bonus: Accept floating point values for amount per day, and round the result to a round number.
+
+function lifeTimeSupply(age, amountPerDay) {
+	var maxAge = 99;
+	var calculation = ((maxAge - age) * (amountPerDay * 365));
+	console.log("You will need " + calculation + " to last you until the ripe old age of " + maxAge);
 }
 
-calculateAge(1978, 2013)
-calculateAge(1953, 2013)
-calculateAge(1995, 2013)
-
-
-
+lifeTimeSupply(56, 3)
+lifeTimeSupply(26, 10)
+lifeTimeSupply(42, 2)
