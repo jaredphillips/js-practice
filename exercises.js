db0572ce7330e516bfb5b6a84dcb492bc2b722ce
Jared Phillips <jaredtphillips@me.com>
2013-06-17 14:59:13 -0400

    Second set of examples - First completed

--- exercises.js
+++ exercises.js
@@ -44,7 +44,20 @@ var lifeTimeSupplyCalculated = (maximumAge - currentAge) * (365 * amountPerDay);
 var lifeTimeSupply = ("You will need " + lifeTimeSupplyCalculated + " " + favouriteSnack + " to last you until the ripe old age of " + maximumAge )
 console.log(lifeTimeSupply)
 
+// Why pay a fortune teller when you can just program your fortune yourself?
 
+// Write a function named tellFortune that:
+// takes 4 arguments: number of children, partner's name, geographic location, job title.
+// outputs your fortune to the screen like so: "You will be a X in Y, and married to Z with N kids."
+// Call that function 3 times with 3 different values for the arguments.
+
+function tellFortune(numChildren, partnerName, location, jobTitle) {
+  console.log("You will be a " + jobTitle + " in " + location + ", and married to " + partnerName + " with " + numChildren + " kids.")
+}
+
+tellFortune(4, "Rachel", "New York", "Programmer")
+tellFortune(1, "Suzy", "Los Angeles", "Movie Star")
+tellFortune(10, "Kim Kardashinon", "New York", "Programmer")
 
 
 
