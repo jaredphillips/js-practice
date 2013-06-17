083c3f98bb6b9cb1a24e8f7a1fdf7ab873041f9d
Jared Phillips <jaredtphillips@me.com>
2013-06-17 16:14:23 -0400

    For loop - example one complete

--- exercises.js
+++ exercises.js
@@ -172,14 +172,30 @@
 // Bonus: Make it handle a few collective nouns like "sheep" and "geese"
 
 
-function pluralize(noun, number) {
-    if (number != 1 && noun != 'sheep' && noun != 'geese') {
-        return number + ' ' + noun + 's';
-    } else {
-        return number + ' ' + noun;
-    }
-}
+// function pluralize(noun, number) {
+//     if (number != 1 && noun != 'sheep' && noun != 'geese') {
+//         return number + ' ' + noun + 's';
+//     } else {
+//         return number + ' ' + noun;
+//     }
+// }
 
-console.log('I have ' + pluralize('cat', 0));
-console.log('I have ' + pluralize('cat', 1));
-console.log('I have ' + pluralize('cat', 2));
+// console.log('I have ' + pluralize('cat', 0));
+// console.log('I have ' + pluralize('cat', 1));
+// console.log('I have ' + pluralize('cat', 2));
+
+// Write a for loop that will iterate from 0 to 20. For each iteration, it will check if 
+// the current number is even or odd, and report that to the screen (e.g. "2 is even").
+
+// for (var i = 0; i <= 20; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i + ' is even');
+//   } else {
+//     console.log(i + ' is odd');
+//   }
+// }
+
+
+
+
+
