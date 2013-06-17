c874975f115a84a2cecccd37d92fc9ddf4586014
Jared Phillips <jaredtphillips@me.com>
2013-06-17 16:11:39 -0400

    If/Else Loops Completed

--- exercises.js
+++ exercises.js
@@ -163,3 +163,23 @@
 // assignGrade(91)
 // assignGrade(87)
 // assignGrade(45)
+
+
+// Write a function named pluralize that:
+// takes 2 arguments, a noun and a number.
+// returns the number and pluralized form, like "5 cats" or "1 dog".
+// Call that function for a few different scores and log the result to make sure it works.
+// Bonus: Make it handle a few collective nouns like "sheep" and "geese"
+
+
+function pluralize(noun, number) {
+    if (number != 1 && noun != 'sheep' && noun != 'geese') {
+        return number + ' ' + noun + 's';
+    } else {
+        return number + ' ' + noun;
+    }
+}
+
+console.log('I have ' + pluralize('cat', 0));
+console.log('I have ' + pluralize('cat', 1));
+console.log('I have ' + pluralize('cat', 2));
